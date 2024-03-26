cd7467dea4401f181c23bc8d1a44b360ad0aa547
insamju300 <insamju300@gmail.com>
2024-03-26 10:43:13 +0900

    cors문제 해결

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 import sys
 import io
@@ -18,6 +19,23 @@ from datetime import datetime
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 @app.get("/test")
 async def read_root():
